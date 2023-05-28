ab7990fc793f1d64b47da34b38140adcc188bebe
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-05-28 21:05:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def summary(model_name, temperature, top_p, freq_penalty):
         if gen_sum:
             # Initialize the OpenAI module, load and run the summarize chain
             llm = OpenAI(model_name=model_name, temperature=temperature)
-            chain = load_summarize_chain(llm, chain_type="stuff")
+            chain = load_summarize_chain(llm, chain_type="refine")
             #search = docsearch.similarity_search(" ")
             summary = chain.run(input_documents=texts)#, question="Write a concise summary within 300 words.")
 
