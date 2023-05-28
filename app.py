3880b695aecaef8dc5a55976700f6d7e39d1d94d
Safiullah Rahu <safiullahrahu1555@gmail.com>
2023-05-28 19:33:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -95,8 +95,12 @@ def summary(model_name, temperature, top_p, freq_penalty):
             tmp_file.write(uploaded_file.getvalue())
             tmp_file_path = tmp_file.name
         # encoding = cp1252
-        loader = CSVLoader(file_path=tmp_file_path, encoding="cp1252")
-        data = loader.load()
+        try:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="cp1252")
+            data = loader.load()
+        except:
+            loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8")
+            data = loader.load()
 
         os.remove(tmp_file_path)
         gen_sum = st.button("Generate Summary")
